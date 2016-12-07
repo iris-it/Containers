6e4fdb4cc83fa875e0dcaca41321c33c00ab8df7
Alexandre Mangin <alexandre.mangin@viacesi.fr>
2016-12-07 14:31:47 +0100

    fixes

--- phpmyadmin/jenkins.sh
+++ phpmyadmin/jenkins.sh
@@ -1,37 +1,44 @@
-#!/usr/bin/env bash
+# SECRETS :
+# DOCKER_USERNAME
+# DOCKER_PASSWORD
+# MARATHON_USER
+# MARATHON_PASSWORD
+# DATACENTER_URL
+# HAPROXY_VHOST
 
-NAME="phpmyadmin"
-DOCKER_USERNAME="user"
-DOCKER_PASSWORD="${REGISTRY_PASSWORD}"
-IMAGE_NAME="${DOCKER_USERNAME}/${NAME}:${GIT_COMMIT}"
+# VARS :
+# PMA_HOST
+# PMA_PORT
 
-APP_ID="/${NAME}"
-HAPROXY_VHOST=""
+#--------------------------------------------------------
+
+cd ${JOB_NAME}
+
+IMAGE_NAME="${DOCKER_USERNAME}/${JOB_NAME}:${GIT_COMMIT}"
+
+docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+docker build -t ${IMAGE_NAME} `pwd -P`
+docker push ${IMAGE_NAME}
+
+#--------------------------------------------------------
+
+cd ${JOB_NAME}
+
+APP_ID="/${JOB_NAME}"
+IMAGE_NAME="${DOCKER_USERNAME}/${JOB_NAME}:${GIT_COMMIT}"
+
+PMA_HOST="192.168.0.20"
+PMA_PORT="3306"
 
 INSTANCE_RAM=64
 INSTANCE_CPU=0.25
 INSTANCE_DISK=0
 INSTANCE_NUMBER=1
 
-PMA_HOST=""
-PMA_PORT=""
+sed -i "s|__DOCKER_IMAGE_NAME__|${IMAGE_NAME}|g" marathon.json
+sed -i "s|__APP_ID__|${APP_ID}|g" marathon.json
+sed -i "s|__HAPROXY_VHOST__|${HAPROXY_VHOST}|g" marathon.json
+sed -i "s|__PMA_HOST__|${PMA_HOST}|g" marathon.json
+sed -i "s|__PMA_PORT__|${PMA_PORT}|g" marathon.json
 
-cp -f marathon.json marathon.json.tmp
-
-sed -i "s|__DOCKER_IMAGE_NAME__|${IMAGE_NAME}|g" marathon.json.tmp
-
-sed -i "s|__APP_ID__|${APP_ID}|g" marathon.json.tmp
-sed -i "s|__HAPROXY_VHOST__|${HAPROXY_VHOST}|g" marathon.json.tmp
-
-sed -i "s|__PMA_HOST__|${PMA_HOST}|g" marathon.json.tmp
-sed -i "s|__PMA_PORT__|${PMA_PORT}|g" marathon.json.tmp
-
-rm marathon.json
-mv marathon.json.tmp marathon.json
-
-docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
-docker build -t ${IMAGE_NAME} `pwd -P`
-docker push ${IMAGE_NAME}
-
-
-curl -X PUT -H "Content-type: application/json" -u admin:${MARATHON_PASSWORD} -d @marathon.json "http://datacenter.com:8080/v2/apps?force=true"
+curl -X PUT -H "Content-type: application/json" -u ${MARATHON_USER}:${MARATHON_PASSWORD} -d @marathon.json "http://${DATACENTER_URL}/v2/apps?force=true"
